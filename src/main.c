2cc50b9bd5f8a28c446dd2d06a2dff1fcfe71df0
Travis Hopkins <trhopkins@protonmail.com>
2021-11-10 16:17:42 -0500

    Added hello world to main
    All c and header files will go in src/ with main.c.
    
    Just a basic program to ensure that compilation with gcc works.

---
+++ src/main.c
@@ -0,0 +1,11 @@
+/**
+ * Snake game developed for CS 355
+ * @author Travis Hopkins
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello world!\n"); // initial code to prove compiler/makefile is working
+	return 0;
+}
