de6e29e1411bf1f135939e1abc8cce2847a8ca6e
ashthephoenix <jtaber27@gmail.com>
2021-11-11 01:40:02 +0000

    added initializepit function to main file

--- src/main.c
+++ src/main.c
@@ -4,13 +4,36 @@
  */
 
 #include <ncurses.h>
-
+int maxcols, maxrows;
+void initializepit();
 int main(void) {
 	initscr();              // start ncurses
-	addstr("Hello world!"); // add message to buffer
-	refresh();              // show buffer
+
+    initializepit();        //create border around "snake pit"
+    refresh();
+
 	getch();                // get user input
+    
 	endwin();               // quit ncurses
 	return 0;               // exit program
+}
+
+/*creates the border around the playing area and sets the global variables maxcols and maxrows to 
+the largest y and x coordinates respectively.
+*/
+void initializepit(){
+    getmaxyx(stdscr, maxcols, maxrows);
+    for(int i = 0; i < maxrows; i ++){
+        move(0,i);
+        printw("-");
+        move(maxcols-1, i);
+        printw("-");
+    }
+    for(int i = 0; i < maxcols; i++){
+        move(i,0);
+        printw("|");
+        move(i, maxrows-1);
+        printw("|");
+    }
 }
 
