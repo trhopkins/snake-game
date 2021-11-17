9c9ba1e07143992e0a7a537d9cc754a43ae0c01c
ashthephoenix <jtaber27@gmail.com>
2021-11-17 21:15:38 +0000

    added makesnake function

--- src/main.c
+++ src/main.c
@@ -4,36 +4,66 @@
  */
 
 #include <ncurses.h>
-int maxcols, maxrows;
+int snakestart = 3; int snakex; int snakey;
+
 void initializepit();
+void makesnake();
+
 int main(void) {
 	initscr();              // start ncurses
-
+    curs_set(FALSE);
+    cbreak();
+    keypad(stdscr, TRUE);
+    
     initializepit();        //create border around "snake pit"
     refresh();
+    makesnake(KEY_RIGHT);
+    refresh();
 
-	getch();                // get user input
-    
+    int gameend = 0;
+	int input = getchar();                // get user input
+    while(gameend){
+        //the game is played or something
+    }
+
 	endwin();               // quit ncurses
 	return 0;               // exit program
 }
 
-/*creates the border around the playing area and sets the global variables maxcols and maxrows to 
-the largest y and x coordinates respectively.
+/*creates the border around the playing area 
 */
 void initializepit(){
-    getmaxyx(stdscr, maxcols, maxrows);
-    for(int i = 0; i < maxrows; i ++){
+    for(int i = 0; i < COLS; i ++){
         move(0,i);
         printw("-");
-        move(maxcols-1, i);
+        move(LINES-1, i);
         printw("-");
     }
-    for(int i = 0; i < maxcols; i++){
+    for(int i = 0; i < LINES; i++){
         move(i,0);
         printw("|");
-        move(i, maxrows-1);
+        move(i, COLS-1);
         printw("|");
+    }
+}
+
+//prints the initial snake on the screen
+void makesnake(int direction){
+    snakex = COLS/2;
+    snakey = LINES/2;    
+    move(snakey, snakex);
+    printw("@");
+    for(int i = 1; i < snakestart; i++){
+        if(direction == KEY_LEFT){
+            move(snakey, snakex + i);
+        }if(direction == KEY_RIGHT){
+            move(snakey, snakex - i);
+        }if(direction == KEY_UP){
+            move(snakey + i, snakex);
+        }if(direction == KEY_DOWN){
+            move(snakey - i, snakex + i);
+        }
+        printw("o");
     }
 }
 
