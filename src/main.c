594233418784c3ac621dc68804f25700abae11f6
ashthephoenix <jtaber27@gmail.com>
2021-11-24 03:18:14 +0000

    created snake struct, updated makesnake to work with the struct, and added the detectcollision function

--- src/main.c
+++ src/main.c
@@ -4,10 +4,20 @@
  */
 
 #include <ncurses.h>
-int snakestart = 3; int snakex; int snakey;
+#include <stdlib.h>
+int snakestart = 3;
 
 void initializepit();
-void makesnake();
+void makesnake(int);
+int detectcollision(int);
+void endgame();
+
+struct snake{
+    int x;
+    int y;
+    struct snake* prev;
+};
+struct snake* head; struct snake* tail;
 
 int main(void) {
 	initscr();              // start ncurses
@@ -19,7 +29,6 @@ int main(void) {
     refresh();
     makesnake(KEY_RIGHT);
     refresh();
-
     int gameend = 0;
 	int input = getchar();                // get user input
     while(gameend){
@@ -47,23 +56,52 @@ void initializepit(){
     }
 }
 
-//prints the initial snake on the screen
+//prints the initial snake on the screen and creates the linked list that comprises the snake
 void makesnake(int direction){
-    snakex = COLS/2;
-    snakey = LINES/2;    
-    move(snakey, snakex);
-    printw("@");
-    for(int i = 1; i < snakestart; i++){
+    head = (struct snake*)malloc(sizeof(struct snake));
+    tail = (struct snake*)malloc(sizeof(struct snake));
+    tail->x = COLS/2;
+    tail->y = LINES/2;
+    struct snake* node= (struct snake*)malloc(sizeof(struct snake));
+    tail->prev = node;
+    struct snake* prevnode= (struct snake*)malloc(sizeof(struct snake));
+    for(int i = 0; i < snakestart ; i++){
         if(direction == KEY_LEFT){
-            move(snakey, snakex + i);
+            node->x = tail->x - i; node->y = tail->y;
         }if(direction == KEY_RIGHT){
-            move(snakey, snakex - i);
+            node->x = tail->x + i; node->y = tail->y;
         }if(direction == KEY_UP){
-            move(snakey + i, snakex);
+            node->x = tail->x; node->y = tail->y - i;
         }if(direction == KEY_DOWN){
-            move(snakey - i, snakex + i);
+            node->x = tail->x; node->y = tail->y + i;
         }
+        move(node->y, node->x);
         printw("o");
+        mvprintw(i + 1, 1, "%d, %d", node->x, node->y);
+        node->prev = prevnode;
+        node = prevnode;
     }
+    head = node; 
+    move(head->y, head->x);
+    printw("@"); 
 }
 
+/*returns 0 if there is no character in the next space, and 1 if there is a character
+*/
+int detectcollision(int direction){
+    int testch;
+    if(direction == KEY_LEFT){
+        testch = mvinch(head->y, head->x + 1);
+    }if(direction == KEY_RIGHT){
+        testch = mvinch(head->y, head->x - 1);
+    }if(direction == KEY_UP){
+        testch = mvinch(head->y + 1, head->x);
+    }if(direction == KEY_DOWN){
+        testch = mvinch(head->y - 1, head->x);
+    }
+    if(testch == ' '){
+        return 0;
+    }else{
+        return 1;
+    }
+}
